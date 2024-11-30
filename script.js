7b1f4940836123b71c8eab3f67da53538a00a178
Marty McLeod <martymcleod@yahoo.com>
2024-11-30 16:41:36 +0800

    Fixed taxiFareApiUrl string value

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+// Uncomment to use my (Marty's) MapBox public token
+//mapboxgl.accessToken = 'pk.eyJ1IjoibWFydHltY2xlb2QiLCJhIjoiY2x6amtpd3pzMDJ6NDJpcHJvejZxZTIwaiJ9.Mm07pY0IZAqNQRGnpppZxw';
+
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
